3a5e9108b1976902c9df389d8426bf096d15806e
Gaspare <gaspare@anlix.io>
2019-01-31 16:23:38 -0200

    Add Support for RTL8367C/S Gigabit Switch

--- package/kernel/linux/modules/netdevices.mk
+++ package/kernel/linux/modules/netdevices.mk
@@ -269,6 +269,20 @@ endef
 
 $(eval $(call KernelPackage,switch-rtl8367b))
 
+define KernelPackage/switch-rtl8367s
+  SUBMENU:=$(NETWORK_DEVICES_MENU)
+  TITLE:=Realtek RTL8367C/S switch support
+  DEPENDS:=+kmod-swconfig
+  KCONFIG:=CONFIG_RTL8367S_GSW
+  FILES:=$(LINUX_DIR)/drivers/net/phy/rtk/rtl8367s_gsw.ko
+  AUTOLOAD:=$(call AutoLoad,43,rtl8367s_gsw)
+endef
+
+define KernelPackage/switch-rtl8367s/description
+ Realtek RTL8367C/S switch support
+endef
+
+$(eval $(call KernelPackage,switch-rtl8367s))
 
 define KernelPackage/natsemi
   SUBMENU:=$(NETWORK_DEVICES_MENU)
