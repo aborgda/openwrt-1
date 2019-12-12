202c54dfe39b64f1e35d396a87f653aae001eca5
Guilherme Senges <guilherme@anlix.io>
2019-12-11 23:42:41 -0300

    Adjusted led blink config to rg1200ac

---
+++ target/linux/realtek/base-files/etc/diag.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# Copyright (C) 2010-2013 OpenWrt.org
+
+. /lib/functions.sh
+. /lib/functions/leds.sh
+
+get_status_led() {
+	board=$(board_name)
+
+	case $board in
+	actionrg1200-v1)
+		status_led="actionrg1200:blue:sys"
+		;;
+	esac
+}
+
+set_state() {
+	get_status_led $1
+
+	case "$1" in
+	preinit)
+		status_led_blink_preinit
+		;;
+	failsafe)
+		status_led_blink_failsafe
+		;;
+	upgrade | \
+	preinit_regular)
+		status_led_blink_preinit_regular
+		;;
+	done)
+		status_led_on
+		;;
+	esac
+}
